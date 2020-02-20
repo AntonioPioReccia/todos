3db6bceaeaafc22b2efa2700dcf0daa3168ad9be
antoniopioreccia <antonioreccia52011@gmail.com>
2020-02-20 13:47:00 +0000

    abbiamo aggiunto i modelli ejs

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require ("express");
 const path = require ("path");
 const app = express ();
 app.listen (3000);
+const todos=[
+{ id: 1,text:"boh",done:false},
+{ id: 6,text:"b5h",done:false},
+{ id: 9,text:"b",done:true}
+];
 
+app.set ("views",path.join(__dirname, "views"));
+app.set("view engine","ejs");
 app.get("/",function (req, res) { 
-  res.send("che vuo tu");
+  res.render("index", {autore: "dante", todos:todos});
 } );
 app.get("/er",function (req, res) { 
   res.send("vernazzaro mio zio");
